f6c7b15672da59a8447bb1540ec4d3dd7c052cb5
aoc00023 <60604059+aoc00023@users.noreply.github.com>
2022-05-18 18:00:55 +0200

    interfaz actualizada

--- Assets/Scripts/InterfaceManager.cs
+++ Assets/Scripts/InterfaceManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,23 +8,44 @@ public class InterfaceManager : MonoBehaviour
 {
     [SerializeField] GameObject menuInicio;
     [SerializeField] GameObject menuPausa;
+    [SerializeField] GameObject menuWin;
+    [SerializeField] GameObject HUD;
+    GameManager gameManager;
+    [SerializeField] TextMeshProUGUI vidas;
+    [SerializeField] TextMeshProUGUI vitalidad;
+    [SerializeField] TextMeshProUGUI puntos;
+    [SerializeField] TextMeshProUGUI tiempo;
+    [SerializeField] TextMeshProUGUI objetos;
+    [SerializeField] TextMeshProUGUI nivel;
 
-    public static bool inicia, pausa;
+    public static bool inicia, pausa, ganar;
 
     // Start is called before the first frame update
     void Start()
     {
         menuInicio.SetActive(false);
         menuPausa.SetActive(false);
+        menuWin.SetActive(false);
+        HUD.SetActive(false);
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
-        inicia = false;
+        gameManager = GameObject.FindObjectOfType<GameManager>();
+        inicia = pausa = ganar = false;
         panelInicio();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+            puntos.text = "Puntos: " + gameManager.GetCurrentScore();
+            vidas.text = "Vidas: " + gameManager.GetLives();
+            vitalidad.text = "Vitalidad: " + gameManager.GetHealth();
+            objetos.text = "Objetos: " + gameManager.GetKeyItems();
+            tiempo.text = "Tiempo: " + (int)Time.timeSinceLevelLoad;
+        
+        
+
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if(pausa)
@@ -35,6 +57,7 @@ public class InterfaceManager : MonoBehaviour
                 panelPausa();
             }
         }
+
     }
 
     public void botonStart()
@@ -43,6 +66,8 @@ public class InterfaceManager : MonoBehaviour
         Cursor.lockState = CursorLockMode.Confined;
         Time.timeScale = 1f;
         menuInicio.SetActive(false);
+        inicia = false;
+        HUD.SetActive(true);
     }
 
     public void botonQuit()
@@ -56,8 +81,15 @@ public class InterfaceManager : MonoBehaviour
         Cursor.lockState = CursorLockMode.Confined;
         menuPausa.SetActive(false);
         pausa = inicia = false;
+        HUD.SetActive(true);
         //menuInicio.SetActive(false);
         Time.timeScale = 1f;
+    }
+
+    public void botonRestart()
+    {
+        SceneManager.LoadScene(0);
+        menuInicio.SetActive(false);
     }
 
     public static bool play()
@@ -67,14 +99,14 @@ public class InterfaceManager : MonoBehaviour
 
     public void panelInicio()
     {
-        if (SceneManager.GetActiveScene().buildIndex == 0)
-        {
-            Cursor.visible = true;
-            Cursor.lockState = CursorLockMode.Confined;
-            inicia = true;
-            menuInicio.SetActive(true);
-            Time.timeScale = 0f;
-        }
+        
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+        inicia = true;
+        menuInicio.SetActive(true);
+        Time.timeScale = 0f;
+
+        nivel.text = "Nivel: " + SceneManager.GetActiveScene();
         
 
     }
@@ -87,6 +119,18 @@ public class InterfaceManager : MonoBehaviour
         menuPausa.SetActive(true);
         Time.timeScale = 0f;
         pausa = true;
-        
+        HUD.SetActive(false);
+
     }
+
+    public void panelWin()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+        menuWin.SetActive(true);
+        ganar = true;
+        
+        Time.timeScale = 0f;
+    }
+
 }
